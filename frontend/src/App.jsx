92a7e05497341664e7ddc8b1bed6a20b9ae60092
wasif-technyx <142510796+wasif-technyx@users.noreply.github.com>
2025-07-08 21:57:13 +0500

    Init commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom'
+import Login from './pages/Auth/Login'
+import SignUp from './pages/Auth/SignUp'
+import Dashboard from './pages/Admin/Dashboard'
+import CreateTask from './pages/Admin/CreateTask'
+import ManageUsers from './pages/Admin/ManageUsers'
+import PrivateRoute from './routes/PrivateRoute'
+import UserDashboard from './pages/User/Dashboard'
+import MyTasks from './pages/User/MyTasks'
+import ViewTaskDetails from './pages/User/ViewTaskDetails'
+
+const App = () => {
+  return (
+    <div className='text-3xl text-rose-600'>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signUp" element={<SignUp />} />
+
+          {/* Admin Routes */}
+          <Route element={<PrivateRoute allowedRoles={["admin"]} />}>
+              <Route path="/admin/dashboard" element={<Dashboard />} />
+              <Route path="/admin/tasks" element={<ManageTasks />} />
+              <Route path="/admin/create-task" element={<CreateTask />} />
+              <Route path="/admin/users" element={<ManageUsers />} />
+          </Route>
+
+          {/* User Routes */}
+          <Route element={<PrivateRoute allowedRoles={["admin"]} />}>
+              <Route path="/user/dashboard" element={<UserDashboard />} />
+              <Route path="/user/tasks" element={<MyTasks />} />
+              <Route path="/user/task-details/:id" element={<ViewTaskDetails />} />
+          </Route>
+          <Route />
+        </Routes>
+      </Router>
+    </div>
+  )
+}
+
+export default App
