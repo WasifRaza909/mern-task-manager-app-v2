39d25ef91ba237e0876fea55fc175bab507e61ae
wasifraza909 <wasifraza909@gmail.com>
2025-11-10 11:57:02 +0500

    Wording on Dashboard, started charts

--- frontend/src/pages/Admin/Dashboard.jsx
+++ frontend/src/pages/Admin/Dashboard.jsx
@@ -8,6 +8,8 @@ import { API_PATHS } from '../../utils/apiPaths'
 import moment from 'moment'
 import { addThousandsSeparator } from '../../utils/helper'
 import InfoCard from '../../components/Cards/InfoCard'
+import { LuArrowRight } from 'react-icons/lu'
+import TaskListTable from '../../components/TaskListTable'
 
 const Dashboard = () => {
   useUserAuth()
@@ -20,16 +22,43 @@ const Dashboard = () => {
   const [pieChartData, setPieChartData] = useState([])
   const [barChartData, setBarChartData] = useState([])
 
+  // Prepare Chart Data
+  const prepareChartData = (data) => {
+    const taskDistribution = data?.taskDistribution || null;
+    const taskPriorityLevels = data?.taskPriorityLevels || null;
+
+    const taskDistributionData = [
+      {status: "Pending", count: taskDistribution?.Pending || 0},
+      {status: "In Progress", count: taskDistribution?.InProgress || 0},
+      {status: "Completed", count: taskDistribution?.Completed || 0},
+    ]
+
+    setPieChartData(taskDistributionData)
+
+    const PriorityLevelData = [
+      {priority: "Low", count: taskPriorityLevels?.Low || 0},
+      {priority: "Medium", count: taskPriorityLevels?.Medium || 0},
+      {priority: "High", count: taskPriorityLevels?.High || 0},
+    ]
+
+    setBarChartData(PriorityLevelData)
+  }
+
   const getDashboardData = async () => {
     try {
       const response = await axiosInstance.get(API_PATHS.TASKS.GET_DASHBOARD_DATA)
 
       if(response.data){
         setDashboardData(response.data)
+        prepareChartData(response.data?.charts || null)
       }
     } catch (error) {
       console.error("Error fetching users:", error) 
     }
+  }
+
+  const onSeeMore = () => {
+    navigate('/admin/tasks')
   }
 
   useEffect(() => {
@@ -61,7 +90,33 @@ const Dashboard = () => {
               value={addThousandsSeparator(dashboardData?.charts?.taskDistribution?.Pending || 0)}
               color="bg-violet-500"
             />
+            <InfoCard
+              label="In Progress Tasks"
+              value={addThousandsSeparator(dashboardData?.charts?.taskDistribution?.InProgress || 0)}
+              color="bg-cyan-500"
+            />
+            <InfoCard
+              label="Completed Tasks"
+              value={addThousandsSeparator(dashboardData?.charts?.taskDistribution?.Completed || 0)}
+              color="bg-lime-500"
+            />
           </div>
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 my-4 md:my-6">
+        <div className="md:col-span-2">
+          <div className="card">
+            <div className="flex items-center justify-between">
+              <h5 className="text-lg">Recent Tasks</h5>
+
+              <button className="card-btn" onClick={onSeeMore}>
+                See All <LuArrowRight className='text-base'/>
+              </button>
+            </div>
+
+            <TaskListTable tableData={dashboardData?.recentTasks || []}/>
+          </div>
+        </div>
       </div>
       </DashboardLayout>
   )
